89d590e404a42bdf3e384d153cbcc680336e6314
Plamen pandev <looopooo45@gmail.com>
2023-10-31 19:54:16 +0200

    First attempt working fine, no bugs are found at this moment

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace WebCrawler
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task StartCralwer()
         {
+            var url = @"https://www.automobile.tn/fr/neuf/audi ";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var HtmlDocument=new HtmlDocument();
+            HtmlDocument.LoadHtml(html);
+
+           var divs= HtmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("versions-item")).ToList();
+            foreach ( var div in divs )
+            {
+                var link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value;
+                Console.WriteLine($"https://www.automobile.tn{link}");
+            }
+        }
+        static void Main(string[] args)
+        {          
+            StartCralwer();
+
+            Console.ReadLine();
         }
     }
 }
